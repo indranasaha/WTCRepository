9ecd2db4f19aeb947536ab6b93432ccdba0dfd9b
indranasaha <indranasaha@yahoo.in>
2021-06-19 23:58:44 +0530

    First commit

--- src/main/java/com/wtc/india/com/wtc/india/App2.java
+++ src/main/java/com/wtc/india/com/wtc/india/App2.java
@@ -4,11 +4,10 @@ package com.wtc.india.com.wtc.india;
  * Hello world!
  *
  */
-public class App 
+public class App2 
 {
     public static void main( String[] args )
     {
-        System.out.println( "WTC final" );
-//        Comment added
+        System.out.println( "WTC final - employee created" );
     }
 }
