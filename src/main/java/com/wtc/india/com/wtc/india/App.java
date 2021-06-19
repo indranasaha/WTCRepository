477af50280986358b439a9b7c886617d645dbb79
indranasaha <indranasaha@yahoo.in>
2021-06-19 23:04:17 +0530

    First commit

---
+++ src/main/java/com/wtc/india/com/wtc/india/App.java
@@ -0,0 +1,13 @@
+package com.wtc.india.com.wtc.india;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "WTC final" );
+    }
+}
